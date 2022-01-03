3697f4cf0efb19705675d962e1dcf26b198e3e1b
luisEdu <luisedu.unifal@gmail.com>
2022-01-03 19:30:31 -0300

    Projeto criado

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import {ToastModule} from 'primeng/toast';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TableModule,
+    ToolbarModule,
+    InputTextModule,
+    InputMaskModule,
+    DropdownModule,
+    SelectButtonModule,
+    ConfirmDialogModule,
+    ProgressSpinnerModule,
+    ToastModule
+  ],
+  providers: [ConfirmationService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
