c8fa84eb07ac808961a24acaa134729ad1503b75
luisEdu <luisedu.unifal@gmail.com>
2022-01-08 08:27:00 -0300

    Criado metodo GETall no component de Posts

--- src/app/business/post/components/post.component.ts
+++ src/app/business/post/components/post.component.ts
@@ -11,9 +11,9 @@ import { BroadCastService } from 'src/app/broadcast.service';
 })
 export class PostComponent implements OnInit {
 
-  filteredClients: Post[] = [];
+  filteredPosts: Post[] = [];
 
-  _clients: Post[] = [];
+  _posts: Post[] = [];
 
   _filterBy: string;
 
@@ -22,11 +22,23 @@ export class PostComponent implements OnInit {
   constructor(private postService: PostService, private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
-
+    this.broadCast();
+    this.getPosts();
   }
 
   getPosts(): void {
-
+    BroadCastService.get("spinner").emit(true);
+    this.postService.listAll().subscribe({
+      next: posts => {
+        this._posts = posts;
+        this.filteredPosts = this._posts;
+        BroadCastService.get("spinner").emit(false);
+      },
+      error: err => {
+        console.log('Error', err);
+        BroadCastService.get("spinner").emit(false);
+      }
+    });
   }
 
   set filter(value: string) {
