2f5697d4e22d8f9918875c1bbd533817cfb54730
luisEdu <luisedu.unifal@gmail.com>
2022-01-08 08:34:08 -0300

    Criado DELETE e ajustes no filtro component Posts

--- src/app/business/post/components/post.component.ts
+++ src/app/business/post/components/post.component.ts
@@ -41,8 +41,29 @@ export class PostComponent implements OnInit {
     });
   }
 
-  set filter(value: string) {
+  deleteById(post_id: number): void {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir esta publicação??',
+      accept: () => {
+        BroadCastService.get("spinner").emit(true);
+        this.postService.deleteById(post_id).subscribe({
+          next: () => {
+            console.log(`Post with id ${post_id} deleted with success. `);
+            this.getPosts();
+          },
+          error: err => {
+            console.log('Error', err);
+            BroadCastService.get("spinner").emit(false);
+          }
+        });
+      }
+    });
+  }
 
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredPosts = this._posts.filter((post: Post) => post.description.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
   }
 
   get filter() {
